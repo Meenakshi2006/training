bd0b661e9d52f7c7b5619cd0632714b8e166e171
deep0133 <deepanshuheer27@gmail.com>
2024-10-17 23:44:55 +0530

    Day 4 covered - flex, setup nodejs, basic of js

---
+++ Day4/basic.js
@@ -0,0 +1,40 @@
+// console.log("------welcome in js-------");
+// console.warn("------warning------------");
+// console.error("------Error------------");
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var a = 10;
+// let b = 10;
+// const c = 10;
+
+// console.log(+"---------" - a, b, c);
+
+const a = 5;
+const b = "5";
+
+// if (a === b) {
+//   console.log("---a-----:", a);
+// } else {
+//   console.log("----b----:", b);
+// }
+
+// switch (a) {
+//   case b:
+//     console.log("value is equal");
+// }
+
+// Functions:
+
+// named function
+function sum(x, y, z) {
+  let ans = x + y + z;
+  console.log("-----in sum function----");
+  return ans;
+}
+
+const rohan = sum(6, 7, 8);
+
+console.log(rohan);
